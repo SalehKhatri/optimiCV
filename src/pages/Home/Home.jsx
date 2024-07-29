862d5a301a3173b9ccfd8081d5040ca818663849
Saleh <salehkhatri29@gmail.com>
2024-07-30 01:25:25 +0530

    FIxed issues

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,8 +4,8 @@ const Home = () => {
   return (
       <div className="flex justify-center h-[100vh] w-[100%] bg-gradient-to-b from-[#7693ef] to-[#7dfccf] via-[#78b1e6]">
         <div className="triangle"></div>
-        <div className="z-10 mt-28 flex flex-col items-center w-[90%] md:w-[100%] text-center">
-          <h1 className="uppercase font-Bebas-Neue text-6xl w-[90%] md:w-[100%]  lg:text-7xl ">ARE YOU STRUGGLING TO GET SHORTLISTED?</h1>
+        <div className="z-10 mt-28 flex flex-col items-center md:w-[100%] px-3 text-center">
+          <h1 className="uppercase font-Bebas-Neue text-5xl md:w-[100%]  lg:text-7xl ">ARE YOU STRUGGLING TO GET SHORTLISTED?</h1>
           <p className="font-LemonMilk font-medium mt-4 text-sm md:text-lg md:mt-12">Did you know that making a custom CV for each job can boost your chances by 87%? But who has time to make hundreds of CVs every month?</p>
           <p className="font-LemonMilk font-medium mt-4 text-sm md:text-lg md:mt-12">Don&apos;t worry! With OptimiCV, you can create a custom CV for any job in just a few seconds. Now you can focus on the quantity of applications, we&apos;ll handle the quality!</p>
 
