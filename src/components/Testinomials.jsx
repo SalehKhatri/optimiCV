3a600a5e50eb7ac3749bea326a677df226053e62
Saleh <salehkhatri29@gmail.com>
2024-08-02 16:05:58 +0530

    Fixed a font sizing and carousel sizing and added animations

--- src/components/Testinomials.jsx
+++ src/components/Testinomials.jsx
@@ -1,68 +1,99 @@
+import { AnimatePresence, motion } from "framer-motion";
 import React, { useState } from "react";
 
 const testimonials = [
   {
     id: 1,
-    content: "Testimonial 1 content goes here...",
-    author: "Author 1",
+    content: "OptimiCV made CV creation quick and easy!",
+    author: "Alex M.",
+    image:
+      "https://media.istockphoto.com/id/1132792394/photo/headshot-of-a-young-adult.webp?b=1&s=170667a&w=0&k=20&c=KfJg-kHVWlAt-2AmgpX7dA04z9sXt1ikcIW2u601Pig=",
   },
   {
     id: 2,
-    content: "Testimonial 2 content goes here...",
-    author: "Author 2",
+    content: "I got more interviews thanks to tailored CVs from OptimiCV.",
+    author: "Jordan P.",
+    image:
+      "  https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeG1jausYBgHebFtAQzx8uB_S0gSAWBd9-YA&s",
   },
   {
     id: 3,
-    content: "Testimonial 3 content goes here...",
-    author: "Author 3",
+    content: "OptimiCV saved me hours and improved my job applications.",
+    author: "Taylor R.",
+    image:
+      "https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg",
   },
   {
     id: 4,
-    content: "Testimonial 4 content goes here...",
-    author: "Author 4",
+    content: "OptimiCV helped me get shortlisted for multiple positions.",
+    author: "Morgan S.",
+    image:
+      "https://media.istockphoto.com/id/507480872/photo/portrait-of-a-mature-man-smiling-at-the-camera.jpg?s=2048x2048&w=is&k=20&c=_EWh-MQQNoPinSpwL8dKv8LwpoJKENarRDY3NeheMD0=",
   },
 ];
 
 const Testinomials = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [direction, setDirection] = useState(1); // Default direction
 
   const prevSlide = () => {
-    const index =
-      currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1;
-    setCurrentIndex(index);
+    setDirection(-1); // Move left
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? testimonials.length - 1 : prevIndex - 1
+    );
   };
 
   const nextSlide = () => {
-    const index =
-      currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1;
-    setCurrentIndex(index);
+    setDirection(1); // Move right
+    setCurrentIndex((prevIndex) =>
+      prevIndex === testimonials.length - 1 ? 0 : prevIndex + 1
+    );
   };
 
   const goToSlide = (index) => {
+    setDirection(index > currentIndex ? 1 : -1); // Determine direction based on index
     setCurrentIndex(index);
   };
 
   return (
-    <div className="relative w-full flex flex-col items-center  mx-auto mt-16 bg-inherit">
-      <p className="font-LemonMilk font-medium">
+    <div className="relative w-full flex flex-col items-center mx-auto mt-16 pb-5 bg-inherit">
+      <p className="font-LemonMilk font-medium text-xs md:text-base">
         &#x2022; Testimonials &#x2022;
       </p>
-      <div
-        style={{
-          borderTopLeftRadius: "25px 12px",
-          borderTopRightRadius: "25px 12px",
-          borderBottomLeftRadius: "25px 12px",
-          borderBottomRightRadius: "25px 12px",
-        }}
-        className="relative bg-white bg-opacity-70 border  border-black rounded-lg shadow-lg p-6 mt-3 max-w-[90%] w-[300px]"
-      >
-        <div className="absolute -top-4 -left-[10px] circle w-14 h-14 rounded-[100%] border border-black bg-white bg-opacity-50"></div>
-        <p className="text-center">{testimonials[currentIndex].content}</p>
-        <p className="text-right mt-4">- {testimonials[currentIndex].author}</p>
-      </div>
-
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={currentIndex}
+          initial={{ opacity: 0, x: direction * 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -direction * 50 }}
+          transition={{ duration: 0.3, ease: "easeOut" }}
+          style={{
+            borderTopLeftRadius: "25px 12px",
+            borderTopRightRadius: "25px 12px",
+            borderBottomLeftRadius: "25px 12px",
+            borderBottomRightRadius: "25px 12px",
+          }}
+          className="relative flex flex-col justify-center items-center bg-white bg-opacity-70 border border-black rounded-lg shadow-xl drop-shadow-md px-6 py-3 mt-3 max-w-[90%] w-[300px] md:w-[400px]"
+        >
+          <div className="absolute -top-4 -left-[10px] w-14 h-14 rounded-full border border-black bg-white bg-opacity-50 overflow-hidden">
+            <img
+              src={testimonials[currentIndex].image}
+              alt="testimonial"
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <div className="pt-8 pb-2">
+            <p className="text-center font-semibold text-xs">
+              {testimonials[currentIndex].content}
+            </p>
+            <p className="text-right mt-2 font-LemonMilk text-slate-500 text-xs">
+              - {testimonials[currentIndex].author}
+            </p>
+          </div>
+        </motion.div>
+      </AnimatePresence>
       <button
-        className="absolute -left-[8%] md:left-0  top-1/2 transform -translate-y-1/2 p-2 bg-white bg-opacity-60 ring-1 ring-gray-600 rounded-full shadow-lg focus:outline-none"
+        className="absolute -left-[8%] md:left-0 lg:left-[25%] top-1/2 transform -translate-y-1/2 p-2 bg-white bg-opacity-60 ring-1 ring-gray-600 rounded-full shadow-lg focus:outline-none"
         onClick={prevSlide}
       >
         <svg
@@ -70,7 +101,7 @@ const Testinomials = () => {
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
-          className="w-4 h-4"
+          className="w-2 h-2 md:w-3 md:h-3 lg:w-5 lg:h-5"
         >
           <path
             strokeLinecap="round"
@@ -82,7 +113,7 @@ const Testinomials = () => {
       </button>
 
       <button
-        className="absolute -right-[8%]  md:right-0 top-1/2 transform -translate-y-1/2 p-2 bg-white bg-opacity-60 ring-1 ring-gray-600 rounded-full shadow-lg focus:outline-none"
+        className="absolute -right-[8%] md:right-0 lg:right-[25%] top-1/2 transform -translate-y-1/2 p-2 bg-white bg-opacity-60 ring-1 ring-gray-600 rounded-full shadow-lg focus:outline-none"
         onClick={nextSlide}
       >
         <svg
@@ -90,7 +121,7 @@ const Testinomials = () => {
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
-          className="w-4 h-4"
+          className="w-2 h-2 md:w-3 md:h-3 lg:w-5 lg:h-5"
         >
           <path
             strokeLinecap="round"
@@ -105,10 +136,10 @@ const Testinomials = () => {
         {testimonials.map((_, index) => (
           <button
             key={index}
-            className={`w-3 h-3 mx-1 rounded-full ${
+            className={`w-2 h-2 mx-1 rounded-full ${
               currentIndex === index
-                ? "transition-transform duration-150 ease-out bg-transparent border-[3px] border-white ring-[1px] ring-black transform -translate-y-[8px]"
-                : "bg-white border-[0.5px] border-black"
+                ? "transition-transform duration-150 ease-out bg-transparent border-[2px] border-white ring-[1px] ring-black transform -translate-y-[8px]"
+                : "bg-white border-[0.25px] border-black"
             }`}
             onClick={() => goToSlide(index)}
           ></button>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,64 @@
 import React from "react";
-import './Home.css'
+import { motion } from "framer-motion";
+import './Home.css';
 import Testinomials from "../../components/Testinomials";
+
 const Home = () => {
   return (
-      <div className="flex flex-col items-center h-[100dvh] w-[100%] bg-gradient-to-b from-[#7693ef] to-[#7dfccf] via-[#78b1e6]">
-        <div className="triangle"></div>
-        <div className="z-10 mt-28 flex flex-col items-center md:w-[100%] px-3 text-center">
-          <h1 className="uppercase font-Bebas-Neue text-5xl md:w-[100%]  lg:text-7xl ">ARE YOU STRUGGLING TO GET SHORTLISTED?</h1>
-          <p className="font-LemonMilk font-medium mt-4 text-sm md:text-lg md:mt-12">Did you know that making a custom CV for each job can boost your chances by 87%? But who has time to make hundreds of CVs every month?</p>
-          <p className="font-LemonMilk font-medium mt-4 text-xs md:text-lg md:mt-8">Don&apos;t worry! With OptimiCV, you can create a custom CV for any job in just a few seconds. Now you can focus on the quantity of applications, we&apos;ll handle the quality!</p>
+    <div className="flex flex-col items-center h-[100dvh] w-[100%] bg-gradient-to-b from-[#7693ef] to-[#7dfccf] via-[#78b1e6]">
+      <div className="triangle"></div>
 
-          <button className="font-LemonMilk font-medium mt-6 text-sm md:text-lg md:mt-12 bg-black py-3 px-4 rounded-lg text-[#78afe6] hover:scale-105 transition-all duration-300 ease-in">Optimise My Resume</button>
-        </div>
-          <div className="z-10 mt-10 flex flex-col items-center md:w-[100%] px-3 text-center space-y-2">
-          <p className="uppercase font-Bebas-Neue text-5xl md:w-[100%]  lg:text-7xl ">OUR USERS SAY THAT:</p>
-          <Testinomials />
-          </div>
+      <div className="z-10 mt-24 flex flex-col items-center md:w-[100%] px-4 text-center">
+        <motion.h1
+          className="uppercase font-Bebas-Neue text-5xl md:w-[100%] lg:text-7xl"
+          initial={{ opacity: 0, y: 30 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, ease: "easeOut" }}
+        >
+          ARE YOU STRUGGLING TO GET SHORTLISTED?
+        </motion.h1>
+
+        <motion.p
+          className="font-LemonMilk font-medium mt-4 text-[10px] px-2 leading-snug md:leading-none md:text-lg md:mt-12"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, ease: "easeOut", delay: 0.3 }}
+        >
+          Did you know that making a custom CV for each job can boost your chances by 87%? But who has time to make hundreds of CVs every month?
+        </motion.p>
+
+        <motion.p
+          className="font-LemonMilk font-medium mt-3 text-[10px] px-2 leading-snug md:text-lg md:mt-12"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, ease: "easeOut", delay: 0.6 }}
+        >
+          Don&apos;t worry! With OptimiCV, you can create a custom CV for any job in just a few seconds. Now you can focus on the quantity of applications, we&apos;ll handle the quality!
+        </motion.p>
+
+        <motion.button
+          className="font-LemonMilk font-medium mt-5 text-sm md:text-lg md:mt-12 bg-black py-2 px-4 rounded-lg text-[#78afe6]"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, ease: "easeOut", delay: 0.9 }}
+        >
+          Optimise My Resume
+        </motion.button>
       </div>
+
+      <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6, delay:1.2, ease: "easeOut" }}
+      className="z-10 mt-7 flex flex-col items-center md:w-[100%] px-4 text-center space-y-2">
+        <p
+          className="uppercase font-Bebas-Neue text-3xl md:text-5xl md:w-[100%] lg:text-7xl"
+        >
+          OUR USERS SAY THAT:
+        </p>
+        <Testinomials />
+      </motion.div>
+    </div>
   );
 };
 
